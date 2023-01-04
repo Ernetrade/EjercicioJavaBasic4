3e0a980b6687e5961ac8be7506e76451ffbb9658
legiónDev <ernetrades@gmail.com>
2023-01-04 05:00:17 -0500

    Ejercicio finalizado

---
+++ src/Ejercicio4.java
@@ -0,0 +1,9 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String[] nombres = {"Ernesto", "Juan", "Jose"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print(nombres[i] + " ");
+        }
+    }
+    
+}
